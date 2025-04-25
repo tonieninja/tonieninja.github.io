3852f830b9097cda3ff913251c2fe473f646b796
tonieninja <jakubsitarszkola@gmail.com>
2025-04-25 21:57:21 +0200

    Last Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import CircleSumGame from './CircleSumGame';
+import React from 'react'
+import CircleSumGame from './CircleSumGame'
 
 export default function App() {
   return (
     <div className="h-screen overflow-hidden">
       <CircleSumGame />
     </div>
-  );
+  )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from 'path'
 export default defineConfig({
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),  // teraz '@' → folder 'src'
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
